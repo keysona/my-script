ceaff99ff343b6c740eb7fc2f6a89638a42b2dcb
keysona <keypython@gmail.com>
2016-06-06 09:45:32 +0800

    When finished, will print the client config

--- shadowsocks/ss一键脚本.sh
+++ shadowsocks/ss一键脚本.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# default port is 443
+SS_SERVER_IP=`curl ipecho.net/plain`
+SS_PORT=443 #`seq 1025 9000 | grep -v -E "$PORTS_USED" | shuf -n 1`
+SS_PASSWORD=`dd if=/dev/urandom bs=32 count=1 | md5sum | cut -c-32`
+
 # install shadowsocks
 apt-get update
 apt-get install -y -qq python-pip python-m2crypto supervisor
 pip install shadowsocks
 
 # install libsodium to support chacha20
-wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+echo 'Download and install chacha20'
+wget -q https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
 tar zxf LATEST.tar.gz
 cd libsodium*
 ./configure
@@ -14,15 +20,12 @@ make && make install
 echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 ldconfig
 
-# default port is 443
-SS_PASSWORD=`dd if=/dev/urandom bs=32 count=1 | md5sum | cut -c-32`
-SS_PORT=443 #`seq 1025 9000 | grep -v -E "$PORTS_USED" | shuf -n 1`
-SS_SERVER_IP=`curl ipecho.net/plain`
-
 # get config file
-wget https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/shadowsocks.json -O /etc/shadowsocks.json
-wget https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/shadowsocks.conf -O /etc/supervisor/conf.d/shadowsocks.conf
-wget https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/local.conf -O /etc/sysctl.d/local.conf
+echo 'Download config file'
+wget -q https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/shadowsocks.json -O /etc/shadowsocks.json
+wget -q https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/shadowsocks.conf -O /etc/supervisor/conf.d/shadowsocks.conf
+wget -q https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/local.conf -O /etc/sysctl.d/local.conf
+wget -q https://raw.githubusercontent.com/keysona/my-script/master/shadowsocks/shadowsocks-client.json -O /tmp/client.json
 
 # optimize shadowsocks
 sysctl --system
@@ -39,4 +42,6 @@ supervisorctl reload
 
 # all finish
 echo "Restart shadowsocks: supervisorctl restart shadowsocks"
-echo "You password: $SS_PASSWORD"
+echo "Your client config file:"
+cat /tmp/client.json | sed -r -e "s/SS_SERVER_IP/$SS_SERVER_IP/" -e "s/SS_PORT/$SS_PORT/" -e "s/SS_PASSWORD/$SS_PASSWORD/"
+rm /tmp/client.json
